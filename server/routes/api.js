385830482834fe52b24ed3ccd5699a17fca21a58
g101hong <g101hong@gmail.com>
2026-06-12 13:30:52 +0900

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,13 @@
-// server/routes/api.js
+/**
+ * @fileoverview server/routes/api.js
+ * @description  API 라우터 통합
+ *
+ * /api/impression  → impression.js
+ * /api/card        → card.js
+ */
+
+'use strict';
+
 import { Router }       from 'express';
 import impressionRouter from './impression.js';
 import cardRouter       from './card.js';
